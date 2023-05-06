6d0137d6603f884c9500ea312e90b095e63a6fc9
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-06 18:57:00 +0200

    formatting

--- src/version_util.js
+++ src/version_util.js
@@ -1,14 +1,18 @@
 class VersionUtil {
-    const fs = require('fs');
+    const
+    fs = require('fs');
 
     // Read the contents of the package.json file
-    const pkgPath = `${__dirname}/../package.json`;
+    const
+    pkgPath = `${__dirname}/../package.json`;
 
     // Read the contents of the package.json file
-    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+    const
+    pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
 
     // Access the version property
-    const version = pkg.version;
+    const
+    version = pkg.version;
 
     static getVersion() {
         return VersionUtil.version;
