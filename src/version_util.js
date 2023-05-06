9cfc960819128699854b7bc3e4800e450568395f
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-06 18:54:10 +0200

    add absolute path

--- src/version_util.js
+++ src/version_util.js
@@ -1,22 +1,14 @@
 class VersionUtil {
-    const
-    fs = require('fs');
-    const
-    path = require('path');
-
-    // Get the absolute path to the root directory of the project
-    const
-    rootDir = path.resolve(__dirname, '../');
-
+    const fs = require('fs');
 
     // Read the contents of the package.json file
-    const
-    pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+    const pkgPath = `${__dirname}/../package.json`;
+
+    // Read the contents of the package.json file
+    const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
 
     // Access the version property
-    const
-    version = pkg.version;
-
+    const version = pkg.version;
     static getVersion() {
         return VersionUtil.version;
     }
