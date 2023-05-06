9b9283988ab1a12e2564d01297495cf11c694d12
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-06 19:16:03 +0200

    add version

--- src/version_util.js
+++ src/version_util.js
@@ -1,21 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 class VersionUtil {
-    const
-    fs = require('fs');
+    // Get the absolute path to the root directory of the project
+    static rootDir = path.resolve(__dirname, '../');
 
     // Read the contents of the package.json file
-    const
-    pkgPath = `${__dirname}/../package.json`;
-
-    // Read the contents of the package.json file
-    const
-    pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+    static pkg = JSON.parse(fs.readFileSync(path.join(VersionUtil.rootDir, 'package.json'), 'utf-8'));
 
     // Access the version property
-    const
-    version = pkg.version;
-
     static getVersion() {
-        return VersionUtil.version;
+        return VersionUtil.pkg.version;
     }
 }
 
