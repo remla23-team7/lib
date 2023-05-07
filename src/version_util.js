7a2f8fdcdecc09d525336f12fded7c60989e8588
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-07 15:29:32 +0200

    add webpack version

--- src/version_util.js
+++ src/version_util.js
@@ -2,7 +2,7 @@ let VERSION = "0.0.1"; // default version that wil lbe replaced during packing (
 
 class VersionUtil {
     static getVersion() {
-        return VERSION || "0.0.1"; // Default version number
+        return VERSION
     }
 }
 
