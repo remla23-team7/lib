b2cc65250fafa547bbdf2867617fd83825b5f77d
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-07 17:08:12 +0200

    add module

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,27 +1,15 @@
-const path = require('path');
+import path from 'path';
 
 module.exports = (env) => {
-    return {
-        mode: 'production',
-        entry: './src/version_util.js',
-        output: {
-            filename: 'version_util.js',
-            path: path.resolve(__dirname, 'dist'),
-            library: 'VersionUtil',
-            libraryTarget: 'umd',
-            umdNamedDefine: true,
-        },
-        resolve: {
-            extensions: ['.ts', '.js', '.d.ts'],
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-    };
+  return {
+    mode: 'production',
+    entry: './src/version_util.js',
+    output: {
+      filename: 'version_util.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'VersionUtil',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    }
+  };
 };
