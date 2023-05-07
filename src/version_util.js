30092815bfbcbbad9896b7ee1fe8233ad8a03b8c
Andrei-Cristian Ionescu <A.C.Ionescu-1@student.tudelft.nl>
2023-05-07 15:35:37 +0200

    try to inject version

--- src/version_util.js
+++ src/version_util.js
@@ -1,8 +1,8 @@
-let VERSION = "0.0.1"; // default version that wil lbe replaced during packing (webpack)
+let VERSION = VERSION;
 
 class VersionUtil {
     static getVersion() {
-        return VERSION
+        return VERSION || "0.0.1"; // Default version number
     }
 }
 
